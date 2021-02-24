e8b8e2bd09ddc8619df5a985ef513b93954c418f
liguoyou <516115887@qq.com>
2021-02-24 10:14:33 +0800

    修改优化

--- vue-examples/src/views/form/Validator.vue
+++ vue-examples/src/views/form/Validator.vue
@@ -1,32 +1,21 @@
 <template>
   <div class="container">
-    <el-form
-      ref="form"
-      :model="formData"
-      :rules="formRules"
-    >
-      <el-form-item
-        label="价格:"
-        prop="price"
-      >
+    <el-form ref="form" :model="formData">
+      <el-form-item label="价格:" prop="price">
         <el-input
           v-model="formData.price"
           type="text"
+          maxlength="17"
           placeholder="只能输入数字、小数点的正数; 最多能输入两位小数"
           @input="handlePriceInput"
         />
       </el-form-item>
-      <el-form-item
-        ref="form"
-        :model="formData"
-        :rules="formRules"
-        label="正整数:"
-        prop="price"
-      >
+      <el-form-item label="正整数:" prop="price">
         <el-input
           v-model="formData.nums"
           type="text"
-          placeholder="只能输入正整数"
+          maxlength="3"
+          placeholder="只能输入正整数; 最多输入3位数"
           @input="(value) => formData.nums = $tools.numberFilter(value)"
         />
       </el-form-item>
@@ -47,9 +36,6 @@ export default {
       formData: {
         price: '',
         nums: '',
-      },
-      formRules: {
-        price: [],
       },
     };
   },
